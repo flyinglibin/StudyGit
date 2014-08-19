9d6fd959bfd248363989535c9cda28b9e1d561c0
flyinglibin <flyinglibin@163.com>
2014-08-19 12:50:22 +0800

    use -a opinion test1.java del something test2.java add something

--- test2.java
+++ test2.java
@@ -1,5 +1,7 @@
 public class test2{
   public static void main(String[] args){
     System.out.println("hello , this is my second test");
+	System.out.println("change some code");
+	System.out.println("change some code 123");
   }
 }
