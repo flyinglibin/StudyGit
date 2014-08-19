5c5a88245017a151c7d71ff746e01a3aaeaa1f0a
flyinglibin <flyinglibin@163.com>
2014-08-19 13:03:45 +0800

    preper to test the opinion mv

--- test1.java
+++ test1.java
@@ -2,5 +2,6 @@ public class test1{
   public static void main(String[] args){
     System.out.println("hello , this is my first test");
 	System.out.println("del  ");
+	System.out.println("hello ,you are my little apples  ");
   }
 }
